6ccec7cdad5152f4c161da503db2ca357987888d
bmeritter <bme_ritter@foxmail.com>
2017-02-13 16:06:08 +0800

    add func

--- src/collect-all-even.js
+++ src/collect-all-even.js
@@ -1,8 +1,6 @@
-function collect_all_even(collection) {
+function collectAllEven(collection) {
   //在这里写入代码
-  return collection.filter(function (c) {
-    return c % 2 === 0 ;
-  })
+  
 }
 
-module.exports = collect_all_even;
+module.exports = collectAllEven;
