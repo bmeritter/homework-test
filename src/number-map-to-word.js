0442c456cc3d1329b9a5a43550055612db18cc80
bmeritter <bme_ritter@foxmail.com>
2017-02-14 13:22:51 +0800

    add answer

--- src/number-map-to-word.js
+++ src/number-map-to-word.js
@@ -1,6 +1,14 @@
 function numberMapToWord(collection) {
   //在这里写入代码
-
+  return collection.map(function (c) {
+    const a = parseInt(c / 26);
+    const b = c % 26;
+    if (a === 0) {
+      return String.fromCharCode(b + 96)
+    } else {
+      return String.fromCharCode(a + 96, b + 96)
+    }
+  });
 }
 
 module.exports = numberMapToWord;
